ba2e553c642f372079f0653d4a9e8c518825153e
konstantinos <kyriazok@csd.auth.gr>
2022-05-17 15:46:32 +0300

    changes

--- classes.py
+++ classes.py
@@ -1,47 +1,60 @@
 from random import randrange
 
-
 class SakClass:
     """
         σακουλάκι με τα γράμματα του παιχνιδιού
     """
     total_words_sak = None
-    lets = {'A': [12, 1], 'Β': [1, 8], 'Γ': [2, 4], 'Δ': [2, 4], 'Ε': [8, 1],
-            'Ζ': [1, 10], 'Η': [7, 1], 'Θ': [1, 10], 'Ι': [8, 1], 'Κ': [4, 2],
-            'Λ': [3, 3], 'Μ': [3, 3], 'Ν': [6, 1], 'Ξ': [1, 10], 'Ο': [9, 1],
-            'Π': [4, 2], 'Ρ': [5, 2], 'Σ': [7, 1], 'Τ': [8, 1], 'Υ': [4, 2],
-            'Φ': [1, 8], 'Χ': [1, 8], 'Ψ': [1, 10], 'Ω': [3, 3]}
+    lets = {'A': [1, 1], 'Β': [1, 8], 'Γ': [1, 4], 'Δ': [1, 4], 'Ε': [1, 1],
+            'Ζ': [1, 10], 'Η': [1, 1], 'Θ': [1, 10], 'Ι': [1, 1], 'Κ': [1, 2],
+            'Λ': [1, 3], 'Μ': [1, 3], 'Ν': [1, 1], 'Ξ': [1, 10], 'Ο': [1, 1],
+            'Π': [1, 2], 'Ρ': [1, 2], 'Σ': [1, 1], 'Τ': [1, 1], 'Υ': [1, 2],
+            'Φ': [1, 8], 'Χ': [1, 8], 'Ψ': [1, 10], 'Ω': [1, 3]}
 
     def __init__(self, number):
         SakClass.total_words_sak = number
 
-    def randomize_sak(self): # ετοιμάζει το σακουλάκι με τα γράμματα
+    def randomize_sak(self):  # ετοιμάζει το σακουλάκι με τα γράμματα
         if SakClass.total_words_sak >= 7:  # αν χωραει να παρει 7 γραμματα απο σακουλι
             letters = list(SakClass.lets)
-            self.letters = [] # τα γραμματα που θα παιξει ο παικτης/Η\Υ
-            self.points = [] # οι ποντοι των γραμματων
+            self.letters = []  # τα γραμματα που θα παιξει ο παικτης/Η\Υ
+            self.points = []  # οι ποντοι των γραμματων
             for i in range(7):  # 7 γραμματα, τυχαια καθε φορα
 
-                random_number = randrange(24)
-                while SakClass.lets.get(letters[random_number])[0] == 0: # οσο υπαρχει το νουμερο, ψαχνει αλλο
+                random_number = randrange(24)  # τυχαιος αριθμος
+                while SakClass.lets.get(letters[random_number])[0] <= 0:  # οσο υπαρχει το νουμερο, ψαχνει αλλο
                     random_number = randrange(24)
 
                 self.letters.append(letters[random_number])
                 self.points.append(SakClass.lets.get(letters[random_number])[1])
-                SakClass.lets.get(letters[random_number])[0] -= 1
+                SakClass.lets.get(self.letters[i])[0] -= 1  # μειωνω την εμφανισει της λεξης
 
-    def getletters(self): # βγάζει από το σακουλάκι με τον παίκτη 7 γραμματα
-        SakClass.total_words_sak -= 7
+    def getletters(self):  # βγάζει από το σακουλάκι με τον παίκτη τα γραμματα που χρειαζεται
+        SakClass.total_words_sak -= 7  # βγαινουν απο το σακουλακι 7 γραμματα
         for i in range(7):
-            print(f'{self.letters[i]} - {self.points[i]}, ', end="")
-
+            print(f'{self.letters[i]} - {self.points[i]} ', end="")
         print()
 
-    def putbackletters(self):
-        SakClass.total_words_sak += 7
+    def putbackletters(self):  # επιστρέφει γράμματα παίκτη στο σακουλάκι
+        SakClass.total_words_sak += 7  # μπαινουν στο σακουλακι 7 γραμματα
         for i in range(7):
-            SakClass.lets.get(self.letters[i])[0] += 1
-        pass  # επιστρέφει γράμματα παίκτη στο σακουλάκι
+            SakClass.lets.get(self.letters[i])[0] += 1  # αυξανω την εμφανισει της λεξης
+
+    def restorecorrectletters(self, answer):  # αντικαταστει τα νεα γραμματα
+        if SakClass.total_words_sak >= len(answer): # αλλαζω μονο τα σωστα γραμματα που εδωσε ο χρηστης
+            SakClass.total_words_sak -= len(answer)
+            for letter in answer:
+                for letter2 in self.letters:
+                    if letter.upper() == letter2: # βρισκω το σωστο γραμμα
+                        random_number = randrange(24) # περνω ενα αλλο τυχαιο γραμμα που να υπαρχει στο λεξικο
+                        letters = list(SakClass.lets)
+                        while SakClass.lets.get(letters[random_number])[0] <= 0:  # οσο υπαρχει το νουμερο, ψαχνει αλλο
+                            random_number = randrange(24)
+
+                        index = self.letters.index(letter) # αλλαζω την τιμη μονο εκεινου του στοιχειου
+                        self.letters[index] = letters[random_number]
+                        self.points[index] = SakClass.lets[letters[random_number]][1]
+                        SakClass.lets[self.letters[index]][0] -= 1
 
 
 class Player:
@@ -61,7 +74,10 @@ class Human(Player):
         περιγράφει το πως παίζει ο παίκτης
     """
 
-    def __init__(self):
+    def __init__(self, name):
+        super().__init__()
+        self.username = name
+        self.round = 0
         self.points = 0
 
     def __repr__(self):
@@ -69,8 +85,8 @@ class Human(Player):
 
     def play(self, sak, words, score):
         while True:
-            answer = input("Δώσε την λέξη ή 'p' (για pass): ")
-            if answer == 'p':
+            answer = input("Δώσε την λέξη ή 'p' (για pass): ").upper()
+            if answer == 'P':
                 break
             counter_flag = 0
             for letter in answer:
@@ -85,9 +101,14 @@ class Human(Player):
                 print('Επέλεξες λάθος γράμματα, προσπάθησε ξανά!')
 
         round_points = 0
-        if answer.upper() == 'P':  # επελεξε pass
+        if answer == 'P':  # επελεξε pass
             sak.putbackletters()
-        elif answer.upper() in words:  # βρηκε σωστη λεξη
+            sak.randomize_sak()
+            self.round += 1
+            print(f'Παίκτης: {self.username} \t Σκορ: {self.points} \t Σακουλάκι: {sak.total_words_sak} \t Γύρος: {self.round}')
+            print(f'Διαθεσιμα γράμματα - Πόντοι: ', end="")
+            sak.getletters()
+        elif answer in words:  # βρηκε σωστη λεξη
             print('Σωστή λέξη - Κέρδισες ', end='')
             for letter in answer:
                 for i in range(len(sak.letters)):
@@ -95,28 +116,73 @@ class Human(Player):
                         round_points += sak.points[i]
 
             score += round_points
-            print(round_points, 'Πόντους, Το Σκορ σου είναι: ', score)
-            input('Enter για συνέχεια')
-            print('--------------------------------------------------')
+            self.points += score
+            print(round_points, 'Πόντους. Το Σκορ σου είναι: ', score)
+            input('Enter για συνέχεια..  ')
+            print('=======================================================')
+            sak.restorecorrectletters(answer)
+            print(SakClass.total_words_sak)
+            self.round += 1
+            print(f'Παίκτης: {self.username} \t Σκορ: {self.points} \t Σακουλάκι: {sak.total_words_sak + 7} \t Γύρος: {self.round}')
+            print(f'Διαθεσιμα γράμματα - Πόντοι: ', end="")
+            for i in range(7):
+                print(f'{sak.letters[i]} - {sak.points[i]} ', end="")
+            print()
         else:  # βρηκε λαθος λεξη
             print('Λάθος λέξη')
+            sak.randomize_sak()
+            self.round += 1
+            print(f'Παίκτης: {self.username} \t Σκορ: {self.points} \t Σακουλάκι: {sak.total_words_sak} \t Γύρος: {self.round}')
+            print(f'Διαθεσιμα γράμματα - Πόντοι: ', end="")
+            sak.getletters()
 
-        return round_points
 
 class Computer(Player):
     """
         περιγράφει το πως παίζει ο υπολογιστής
     """
 
-    def __init__(self):
-        pass
+    def __init__(self, difficulty):
+        super().__init__()
+        self.round = 0
+        self.points = 0
+        self.difficulty = difficulty
 
     def __repr__(self):
         pass
 
-    def play(self):
-        pass
+    def play(self, sak, words, score):
+        import itertools
+        total_letters = ''
+        for letter in sak.letters:
+            total_letters += letter
+        if self.difficulty == '1': # easy mode
+            combination = 1
+            while True:
+                pc_word = ''
+                combination += 1
+                result_list = list(itertools.permutations(total_letters, combination))
+                for result in result_list:
+                    string = ''
+                    for char in result:
+                        string += char
+                    if string.upper() in words: # found first correct word
+                        pc_word = result
+                        break
 
+                if pc_word != '':
+                    break
+
+            pc_points = 0
+            for char in pc_word:
+                index_of_pc_word_in_sak = sak.letters.index(char)
+                pc_points += sak.points[index_of_pc_word_in_sak]
+            print(pc_word)
+            print(pc_points)
+        elif self.difficulty == '2': # hard mode
+            pass
+        elif self.difficulty == '3': # expert mode
+            pass
 
 class Game:
     """
--- main-3597.py
+++ main-3597.py
@@ -1,6 +1,22 @@
 import classes
 
+def main_menu():
+    print("------------------------------")
+    print("Επέλεξε μια απο τις παρακάτω επιλογές (1,2,3,q)")
+    print('\t 1. Σκορ')
+    print('\t 2. Ρυθμίσεις')
+    print('\t 3. Παιχνίδι')
+    print('\t q. Έξοδος')
+    print("------------------------------")
 
+def settings_menu(first_message, second_message, third_message):
+    print('2. Ρυθμίσεις')
+    print('\tΔυσκολία Υ\Η:')
+    print('\t\t ', first_message)
+    print('\t\t ', second_message)
+    print('\t\t ', third_message)
+    print('<= Back (b)')
+    print("------------------------------")
 def message(username, score, letters, round):
     print(f'Παίκτης: {username} \t Σκορ: {score} \t Σακουλάκι: {letters} \t Γύρος: {round}')
     print(f'Διαθεσιμα γράμματα - Πόντοι: ', end="")
@@ -17,39 +33,77 @@ with open('greek7.txt', 'r') as f7:
     for line in f7:
         words.append(line.strip('\n'))
 
-print('\tΚαλώς ήρθες στο SCRABBLE')
-print("------------------------------")
-print("Επέλεξε μια απο τις παρακάτω επιλογές (1,2,3,q)")
-print('\t 1. Σκορ')
-print('\t 2. Ρυθμίσεις')
-print('\t 3. Παιχνίδι')
-print('\t q. Έξοδος')
-print("------------------------------")
-
-user_input = input('Βάλε την επιλογή σου εδώ: ')
-while user_input != '1' and user_input != '2' and user_input != '3' and user_input.upper() != 'Q':
-    user_input = input()
-
-if user_input.upper() == 'Q':
-    exit()
-elif user_input == '1':
-    print('Score')
-elif user_input == '2':
-    print('Settings')
-else:
-    username = input('Ποιό είναι το username σου: ')
-
-    scorePlayer = 0
-    scorePC = 0
-    round = 0
-    sak = classes.SakClass(50)
-    human = classes.Human()
-    pc = classes.Computer()
-
+pc_difficulty = '1'
+while True: # το παιχνιδι σταματαει μονο οταν ο παικτης δωσει 'q' ή ληξει το παιχνιδι
+    print('\tΚαλώς ήρθες στο SCRABBLE')
+    main_menu()
+    user_input = input('Βάλε την επιλογή σου εδώ: ')
     while True:
+        if user_input == '1' or user_input == '2' or user_input == '3' or user_input.upper() == 'Q':
+            break
+        else:
+            user_input = input('Δώσε μια από τις διαθέσιμες επιλογές (1, 2, 3, q): ')
+
+    if user_input.upper() == 'Q':
+        exit()
+    elif user_input == '1':
+        print("------------------------------")
+        print('Score')
+    elif user_input == '2':
+        first_message = '1)Εύκολο (MIN)'
+        second_message = '2)Δύσκολο (MAX)'
+        third_message = '3)Expert (SMART)'
+        if pc_difficulty == '1':
+            first_message = '--> 1)Εύκολο (MIN)'
+        elif pc_difficulty == '2':
+            second_message = '--> 2)Δύσκολο (MAX)'
+        else:
+            third_message = '--> 3)Expert (SMART)'
+
+        settings_menu(first_message, second_message, third_message)
+        pc_lvl = input("Δώσε το lvl του υπολογιστή η 'b' (για back): ")
+        while True:
+            if pc_lvl == '1':
+                pc_difficulty = '1'
+                first_message = '--> 1)Εύκολο (MIN)'
+                second_message = '2)Δύσκολο (MAX)'
+                third_message = '3)Expert (SMART)'
+                settings_menu(first_message, second_message, third_message)
+                pc_lvl = input("Δώσε το lvl του υπολογιστή η 'b' (για back): ")
+            elif pc_lvl == '2':
+                pc_difficulty = '2'
+                first_message = '1)Εύκολο (MIN)'
+                third_message = '3)Expert (SMART)'
+                second_message = '--> 2)Δύσκολο (MAX)'
+                settings_menu(first_message, second_message, third_message)
+                pc_lvl = input("Δώσε το lvl του υπολογιστή η 'b' (για back): ")
+            elif pc_lvl == '3':
+                pc_difficulty = '3'
+                first_message = '1)Εύκολο (MIN)'
+                second_message = '2)Δύσκολο (MAX)'
+                third_message = '--> 3)Expert (SMART)'
+                settings_menu(first_message, second_message, third_message)
+                pc_lvl = input("Δώσε το lvl του υπολογιστή η 'b' (για back): ")
+            elif not pc_lvl == 'b':
+                pc_lvl = input('Δώσε μια από τις διαθέσιμες επιλογές (1, 2, 3, b): ')
+            elif pc_lvl == 'b':
+                break
+
+        main_menu()
+    elif user_input == '3':
+        username = input('Ποιό είναι το username σου: ')
+        scorePlayer = 0
+        scorePC = 0
+        round = 0
+        sak = classes.SakClass(43)
+        human = classes.Human(username)
+        pc = classes.Computer(pc_difficulty)
         sak.randomize_sak()
-        message(username, scorePlayer, classes.SakClass.total_words_sak, round)
+        print(f'Παίκτης: {username} \t Σκορ: {scorePlayer} \t Σακουλάκι: {sak.total_words_sak} \t Γύρος: {round}')
+        print(f'Διαθεσιμα γράμματα - Πόντοι: ', end="")
         sak.getletters()
 
-        scorePlayer += human.play(sak, words, scorePlayer)
-        round += 1
+        while True:
+            human.play(sak, words, scorePlayer)
+            pc.play(sak, words, scorePC)
+            round += 1
